225598f5d91dcb22984c1e259e3c1177237a29e6
Rory Abraham <rory@expensify.com>
2021-08-18 12:00:26 -0700

    Bring in review data

--- getGitHubContributions.js
+++ getGitHubContributions.js
@@ -34,6 +34,9 @@ const startDate = moment.tz(`${argv.date} 00:00:00`, 'America/Los_Angeles')
     .format(GITHUB_TIMEZONE_FORMAT);
 const endDate = moment.tz(`${argv.date} 23:59:59`, 'America/Los_Angeles')
     .format(GITHUB_TIMEZONE_FORMAT);
+const twoWeeksBefore = moment.tz(`${argv.date} 00:00:00`, 'America/Los_Angeles')
+    .subtract(14, 'days')
+    .format(GITHUB_TIMEZONE_FORMAT);
 
 // Setup Octokit
 const OctokitThrottled = Octokit.plugin(throttling);
@@ -62,6 +65,10 @@ function getGitHubData() {
                 q: `org:Expensify author:${username} created:${startDate}..${endDate}`,
             }),
             octokit.paginate(octokit.search.issuesAndPullRequests, {
+                q: `org:Expensify is:pr reviewed-by:${username} created:${twoWeeksBefore}..${endDate}`,
+                per_page: 100,
+            }),
+            octokit.paginate(octokit.search.issuesAndPullRequests, {
                 q: `org:Expensify commenter:${username} updated:${startDate}..${endDate}`,
             }),
             octokit.paginate(octokit.search.commits, {
@@ -70,20 +77,42 @@ function getGitHubData() {
         ]))
         .then(([
             issuesAndPullRequestsCreated,
+            reviewedPRs,
             issuesAndPullRequestsCommented,
             commits,
         ]) => {
-            return Promise.all(
-                _.map(
-                    issuesAndPullRequestsCommented,
-                    issue => octokit.paginate(`GET ${issue.comments_url.slice('https://api.github.com'.length)}`))
+            return Promise.all(_.map(
+                issuesAndPullRequestsCommented,
+                issue => octokit.paginate(`GET ${issue.comments_url.slice('https://api.github.com'.length)}`))
             )
                 .then(comments => _.filter(_.flatten(comments), comment => comment.user.login === username))
                 .then(comments => ({
                     issues: issuesAndPullRequestsCreated,
+                    reviewedPRs: _.filter(reviewedPRs, reviewedPR => reviewedPR.user.login !== username),
                     comments,
                     commits,
                 }));
+        })
+        .then(({
+            issues,
+            reviewedPRs,
+            comments,
+            commits,
+        }) => {
+            return Promise.all(_.map(
+                reviewedPRs,
+                reviewedPR => octokit.paginate(
+                    `GET ${reviewedPR.url.slice('https://api.github.com'.length)}/timeline`,
+                    {headers: {Accept: 'application/vnd.github.mockingbird-preview'}}
+                )
+            ))
+                .then(events => _.filter(events, event => event.event === 'reviewed' && event.user.login === username))
+                .then(reviews => ({
+                    issues,
+                    reviews,
+                    comments,
+                    commits,
+                }))
         })
         .catch((e) => {
             console.error('Error: Unexpected GitHub API error –', e);
@@ -94,10 +123,15 @@ function getGitHubData() {
 // Format date to match 10am output
 const outputDate = moment(argv.date).format('MMM Do YYYY dddd').toUpperCase();
 getGitHubData()
-    .then(({issues, commits, comments}) => {
+    .then(({issues, reviews, commits, comments}) => {
         let output = `\n${outputDate} [Note: GH Activity]\n`;
-        _.each(issues, issue => output += `• GH: [${issue.pull_request ? 'PR' : 'Issue'} #${issue.number}](${issue.html_url}) – ${issue.title}\n`)
-        output += `• GH: Commits – [\n\t• ${_.pluck(commits, 'html_url').join('\n\t• ')}\n  ]\n`;
-        output += `• GH: Comments – [\n\t• ${_.pluck(comments, 'html_url').join('\n\t• ')}\n  ]\n`;
+        _.each(issues, issue => output += `• GH: [${issue.pull_request ? 'PR' : 'Issue'} #${issue.number}](${issue.html_url}) – ${issue.title}\n`);
+        _.each(reviews, review => output += `• GH: [Reviewed PR #${review.pull_request_url.split('/').pop()}](${review.html_url})`)
+        if (!_.isEmpty(commits)) {
+            output += `• GH: Commits – [\n\t• ${_.pluck(commits, 'html_url').join('\n\t• ')}\n  ]\n`;
+        }
+        if (!_.isEmpty(comments)) {
+            output += `• GH: Comments – [\n\t• ${_.pluck(comments, 'html_url').join('\n\t• ')}\n  ]\n`;
+        }
         console.log(output);
     });
